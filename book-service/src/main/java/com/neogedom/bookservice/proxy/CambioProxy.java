f85262d499f22e5ac0b7453c5998e6e6947da1c4
Vinicius Gomes <neogedom@gmail.com>
2022-09-27 23:29:55 -0400

    section about feign and flyway finished

---
+++ book-service/src/main/java/com/neogedom/bookservice/proxy/CambioProxy.java
@@ -0,0 +1,14 @@
+package com.neogedom.bookservice.proxy;
+
+import com.neogedom.bookservice.response.Cambio;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "cambio-service", url = "localhost:8000")
+public interface CambioProxy {
+    @GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
+    Cambio getCambio(@PathVariable("amount") Double amount, @PathVariable("from") String from, @PathVariable("to") String to);
+}
--- cambio-service/src/main/java/com/neogedom/cambioservice/model/Cambio.java
+++ cambio-service/src/main/java/com/neogedom/cambioservice/model/Cambio.java
@@ -1,40 +1,46 @@
 package com.neogedom.cambioservice.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
-
+@Entity(name = "cambio")
 public class Cambio implements Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String from;
-    private String to;
-    private BigDecimal conversionFactor;
-    private BigDecimal convertedValue;
-    private String environment; 
 
+    @Column(name = "from_currency", nullable = false, length = 3)
+    private String from;
+
+    @Column(name = "to_currency", nullable = false, length = 3)
+    private String to;
+
+    @Column(nullable = false)
+    private BigDecimal conversionFactor;
+
+    @Transient
+    private BigDecimal convertedValue;
+
+    @Transient
+    private String environment;
     public Long getId() {
         return id;
     }
-
 
     public void setId(Long id) {
         this.id = id;
     }
 
-
     public String getFrom() {
         return from;
     }
-
 
     public void setFrom(String from) {
         this.from = from;
     }
 
-
     public String getTo() {
         return to;
     }
-
 
     public void setTo(String to) {
         this.to = to;
@@ -142,5 +148,5 @@ public class Cambio implements Serializable {
         return true;
     }
 
-    
+
 }
