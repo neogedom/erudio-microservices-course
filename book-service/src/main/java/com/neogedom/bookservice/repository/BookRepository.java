995566a4b977fff210eaec1def3c31795e4973f8
Vinicius Gomes <neogedom@gmail.com>
2022-10-03 16:01:23 -0400

    removing microservices to fixing

--- book-service/src/main/java/com/neogedom/bookservice/repository/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package com.neogedom.bookservice.repository;
-
-import com.neogedom.bookservice.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> { }
--- cambio-service/src/main/java/com/neogedom/cambioservice/repository/CambioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.neogedom.cambioservice.repository;
-
-import com.neogedom.cambioservice.model.Cambio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CambioRepository extends JpaRepository<Cambio, Long> {
-    Cambio findByFromAndTo(String from, String to);
-}
