224389a0de0c93652821972b6d527c5a92dad2c8
Vinícius Gomes <neogedom@gmail.com>
2022-10-05 22:28:38 -0400

    conecting services using eureka

--- book-service/src/main/java/com/neogedom/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/neogedom/bookservice/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.neogedom.bookservice.controller;
 
 import com.neogedom.bookservice.model.Book;
+import com.neogedom.bookservice.proxy.CambioProxy;
 import com.neogedom.bookservice.repository.BookRepository;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,17 +17,20 @@ public class BookController {
 
     final private Environment environment;
     final private BookRepository repository;
+    final private CambioProxy proxy;
 
-    public BookController(Environment environment, BookRepository repository) {
+    public BookController(Environment environment, BookRepository repository, CambioProxy proxy) {
         this.environment = environment;
         this.repository = repository;
+        this.proxy = proxy;
     }
     @GetMapping("/{id}/{currency}")
     public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency ) {
         var book = repository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+        var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
         var port = environment.getProperty("local.server.port");
-        book.setCurrency(currency);
-        book.setEnvironment(port);
+        book.setEnvironment(port + " FEIGN");
+        book.setPrice(cambio.getConvertedValue());
         return book;
     }
 }
